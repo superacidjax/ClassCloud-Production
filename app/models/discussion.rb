f3e3f9e8b3e84d8d98624c3660d2a4e9bf65ed20
Brian Dear <superacidjax@me.com>
2011-10-11 00:36:23 +0800

    styling of discussions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -8,4 +8,5 @@ class Discussion < ActiveRecord::Base
   belongs_to :class_room
   validates :title, :description, :user_id, :class_room_id, :presence => true
   validates :title, :uniqueness => {:scope => [:user_id, :class_room_id]}
+  acts_as_textiled :description
 end
